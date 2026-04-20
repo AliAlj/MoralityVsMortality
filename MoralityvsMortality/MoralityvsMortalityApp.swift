3009b45d4c577553bd354ac0393df2566e056fc0
AliAlj <alimjahmi@gmail.com>
2026-04-20 13:18:45 -0400

    Initial Commit

---
+++ MoralityvsMortality/MoralityvsMortalityApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoralityvsMortalityApp.swift
+//  MoralityvsMortality
+//
+//  Created by Ali Aljahmi on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct MoralityvsMortalityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoralityvsMortalityTests/MoralityvsMortalityTests.swift
@@ -0,0 +1,18 @@
+//
+//  MoralityvsMortalityTests.swift
+//  MoralityvsMortalityTests
+//
+//  Created by Ali Aljahmi on 4/20/26.
+//
+
+import Testing
+
+struct MoralityvsMortalityTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
